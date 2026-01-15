e154063dcf4b75ea95027dfe114035bbe5a9ffe1
Dan Gilleland <dgilleland@nait.ca>
2026-01-15 09:43:32 -0700

    Final version (with .innerHTML)
    Homework: Use document.createElement() instead of .innerHTML

--- src/A/review-break-activities-example-START/js/index.js
+++ src/A/review-break-activities-example-START/js/index.js
@@ -17,17 +17,18 @@ document
         // \_form__/ \______/\_ indexer _/
         //              |- collection of element/controls in my form
         console.log(topicInput.value);
+        let topic = topicInput.value.trim();
 
-        let myForm = ev.target;
-        let controls = myForm.element;
-        // When you are reading code, ask yourself: What is it?
-        // The `it` might be a variable/object (What is its DATA TYPE)
-        // or `it' might be a function call 
-        // or it might be some other part of the JavaScript grammar
-
-        // Although it's a "poor choice", we will look for the
-        // DOM element by its CSS class name
-        let ulElement = document.querySelector('.topics-list');
-        ulElement.innerHTML += 
-            `<li class="list-group-item">${topicInput.value}</li>`;
+        if(topic === '') {
+            // Bad input....
+            topicInput.classList.add('is-invalid');
+        } else {
+            // Good input
+            topicInput.classList.remove('is-invalid');
+            let ulElement = document.querySelector('.topics-list');
+            ulElement.innerHTML += 
+                `<li class="list-group-item">${topic}</li>`;
+            topicInput.value = ''; // clear the input
+        }
+        topicInput.focus(); // so that the user can continue....
     });
