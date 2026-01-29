8688b2458031f439911da778431c48507b962a5b
Dan Gilleland <dgilleland@nait.ca>
2026-01-29 08:57:13 -0700

    Demonstrate mapping an array of data inside JSX

--- src/B/nextjs-component-lists-START/simpsons-list/components/SimpsonsCharacters.js
+++ src/B/nextjs-component-lists-START/simpsons-list/components/SimpsonsCharacters.js
@@ -1,6 +1,25 @@
 // ~/components/SimpsonsCharacters.js
+const SIMPSON_CHARACTERS = [
+	"Homer Simpson",
+	"Bart Simpson",
+	"Marge Simpson",
+	"Mr. Burns",
+	"Lisa Simpson",
+	"Apu Nahasapeemapetilon",
+	"Sideshow Bob",
+	"Milhouse Van Houten",
+	"Ned Flanders",
+]
+
 export default function SimpsonsCharacters() {
     return <ul>
-        
+        <li>
+            There are {SIMPSON_CHARACTERS.length} main characters.
+        </li>
+        {
+            SIMPSON_CHARACTERS.map((characterName, index) => <li key={index} id={'char-' + (index + 1)} data-key={index}>
+                {characterName}
+            </li>)
+        }
     </ul>
 }
