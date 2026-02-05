f10176e1517d37eabe91f621b8a362489cfa9168
Dan Gilleland <dgilleland@nait.ca>
2026-02-05 06:58:14 -0700

    Completed SASS demo

--- src/C/nextjs-sass-modules-START/sass-lists-example/next.config.js
+++ src/C/nextjs-sass-modules-START/sass-lists-example/next.config.js
@@ -1,6 +1,15 @@
+const path = require('path'); // This is an example of Common JS importing
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // For the SASS pre-processing,
+  sassOptions: {
+    // look under the ~/styles folder of this project's location
+    includePaths: [path.join(__dirname, 'styles')]
+    //                       \_______/
+    //                      global node variable
+  }
 }
 
 module.exports = nextConfig
