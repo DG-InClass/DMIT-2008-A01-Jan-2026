f1265f49ffcfa563ea1a5e10268214216553f9cb
Dan Gilleland <dgilleland@nait.ca>
2026-02-05 09:44:26 -0700

    Preserving state in my TodoList component

--- src/D/nextjs-event-state-fundamentals-START/state-and-event/components/TodoList.js
+++ src/D/nextjs-event-state-fundamentals-START/state-and-event/components/TodoList.js
@@ -1,5 +1,11 @@
 // ~/components/TodoList.js
 import {Box, Button, Grid, TextField} from '@mui/material';
+// React's useState hook allows us to manage
+// the internal state/data of our component
+// in a way that the React framework can also
+// notice and handle the "re-render" whenever
+// state changes
+import { useState } from 'react';
 
 // React components are designed to have a
 // single parameter, commonly called "props".
@@ -10,8 +16,21 @@ import {Box, Button, Grid, TextField} from '@mui/material';
 // if you want to identify specific property
 // names for the props.
 export default function TodoList(props) {
+    // Using the useState hook is a matter
+    // of applying Array Destructuring syntax.
+    // useState is important in order to hook
+    // into React's rendering processes.
+    const [todoText, setTodoText] = useState('');
+    //     \______/  \_________/             \/
+    //      getter   setter func              |
+    //                       initial value   -|
+
     const onTodoTextChange = (event) => {
         console.log(event.target.value);
+        setTodoText(event.target.value);
+    }
+    const onAddTodoClick = () => {
+        console.log('Clicked the button');
     }
 
     return <>
@@ -21,6 +40,14 @@ export default function TodoList(props) {
             variant="standard"
             sx={ {width: '100%'} }
             onChange={onTodoTextChange}
+            value={todoText}
         />
+        <Button
+            variant='contained'
+            onClick={onAddTodoClick}
+        >Add Todo</Button>
+        <Grid item xs={12}>
+            Current input text: {todoText}
+        </Grid>
     </>
 }
