eccc817af0b1e3318a562d73ab33acdd85d187ce
Dan Gilleland <dgilleland@nait.ca>
2026-02-05 06:58:42 -0700

    Completed MUI Theming demo

---
+++ src/C/nextjs-mui-theming-START/mui-theme-example/utils/theme/config.js
@@ -0,0 +1,28 @@
+// ~/utils/theme/config.js
+import { createTheme } from '@mui/material';
+
+
+const theme = createTheme({
+        palette: {
+            primary: {
+                light: '#757ce8',
+                main: '#3f50b5',
+                dark: '#002884',
+                contrastText: '#fff',
+            },
+            secondary: {
+                light: '#ff7961',
+                main: '#f44336',
+                dark: '#ba000d',
+                contrastText: '#000'
+            },
+        },
+        typography: {
+            h2: {
+                fontFamily: 'Lato',
+            },
+            
+        },
+
+    });
+export { theme }
